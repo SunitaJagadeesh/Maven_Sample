56eb22d413d9f8b6c0042c07ec6c59342049724e
Sunita <sunita@Sunitas-MacBook-Air.local>
2020-03-26 22:48:03 +0530

    Project files added.

---
+++ src/test/java/test/cases/TestNG.java
@@ -0,0 +1,43 @@
+package test.cases;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class TestNG {
+	
+	
+
+		@BeforeClass ()
+		public static void  BeforeClass () {
+			System.out.println("BeforeClass method executed");
+		}
+		@AfterClass ()
+		public static void  AfterClass  () {
+			System.out.println("AfterClass  method executed");
+		}
+		@BeforeMethod ()
+		public static void  BeforeMethod () {
+			System.out.println("BeforeMethod method executed");
+		}
+		@AfterMethod ()
+		public static void  AfterMethod () {
+			System.out.println("AfterMethod method executed");
+		}
+		@Test ()
+		public static void  TC01 () {
+			System.out.println("TC01 method executed");
+		}
+		@Test ()
+		public static void  TC02 () {
+			System.out.println("TC02 method executed");
+		}
+		@Test ()
+		public static void  TC03 () {
+			System.out.println("TC03 method executed");
+		}
+	}
+
+
