bb4d672958965572d6ae471a2604e062d333fa08
Sunita <sunita@Sunitas-MacBook-Air.local>
2020-04-07 22:47:37 +0530

    HRM_Test class added.

---
+++ src/test/java/ToTestJenkins/HRM_Test.java
@@ -0,0 +1,43 @@
+package ToTestJenkins;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class HRM_Test {
+	
+	public static void main(String[] args) throws InterruptedException {
+		
+		System.setProperty("webdriver.chrome.driver","/Users/sunita/Documents/eclipse/BasicWebdriverProgram/chromedriver");
+		
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);     
+		driver.get("http://testingmasters.com/hrm");
+		
+		driver.findElement(By.xpath("//input[@id='txtUsername']")).sendKeys("user02");
+		driver.findElement(By.xpath("//input[@id='txtPassword']")).sendKeys("TM1234");
+		driver.findElement(By.xpath("//input[@id='btnLogin']")).click();
+		
+		Thread.sleep(2000);
+		
+		//mouse hover using Action class
+		Actions act = new Actions(driver);
+		WebElement leave = driver.findElement(By.xpath("//a[@id='menu_leave_viewLeaveModule']"));
+		act.moveToElement(leave).build().perform();
+		
+		driver.findElement(By.xpath("//a[@id='menu_leave_applyLeave']")).click();
+		
+		if (driver.findElement(By.xpath("//h1[text()='Apply Leave']")).isDisplayed())
+			System.out.println("Apply leave page displayed");
+		else
+			System.out.println("Apply leave not page displayed");
+	
+
+	}
+
+}
